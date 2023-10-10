1a20f212d9d1d83c6b323b2b8cfa8b0dcbb0c664
aslan <aslan@DESKTOP-RKE9BUH>
2023-10-10 23:25:44 +0300

    Добавьте файлы проекта.

---
+++ laba1asa/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba1asa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14;
+            Console.Write("Введите внутренний радиус кольца:");
+            double r1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите внешний радиус кольца:");
+            double r2 = Convert.ToDouble(Console.ReadLine());
+            double s = pi * (Math.Pow(r2, 2) - Math.Pow(r1, 2));
+            Console.WriteLine(s);
+        }
+    }
+}
